c4db5e52779260bbb945a0df83ead42918fd682e
Natalia Gherardi <natirgh@gmail.com>
2024-02-25 13:45:33 -0300

    feat(animals): add animals data and delete readme

--- src/api/animals.controller.ts
+++ src/api/animals.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express'
+import { animalsData } from '../data/animals-data'
 
 export const getAnimals = (_req: Request, res: Response): void => {
-  res.status(200).json(['panda', 'dog', 'cat'])
+  res.status(200).json(animalsData)
 }
---
+++ src/data/animals-data.ts
@@ -0,0 +1,12 @@
+export const animalsData = [
+  { animal: 'dog', type: 'mammal' },
+  { animal: 'cat', type: 'mammal' },
+  { animal: 'eagle', type: 'bird' },
+  { animal: 'shark', type: 'fish' },
+  { animal: 'lion', type: 'mammal' },
+  { animal: 'elephant', type: 'mammal' },
+  { animal: 'penguin', type: 'bird' },
+  { animal: 'dolphin', type: 'mammal' },
+  { animal: 'snake', type: 'reptile' },
+  { animal: 'frog', type: 'amphibian' }
+]
