1c43c3e8a4168f3a819b3e790296e1839381fbaa
Natalia Gherardi <natirgh@gmail.com>
2024-02-25 19:04:08 -0300

    feat(animals): add intentional issues to controller

--- src/api/animals.controller.ts
+++ src/api/animals.controller.ts
@@ -1,6 +1,12 @@
 import type { Request, Response } from 'express'
 import { animalsData } from '../data/animals-data'
 
+const animalsDataReceived = [...animalsData]
+
+if (typeof animalsData = 'array') {
+  animalsData.sort()
+}
+
 export const getAnimals = (_req: Request, res: Response): void => {
   res.status(200).json(animalsData)
 }
