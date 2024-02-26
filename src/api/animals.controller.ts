cd290f61851bc13ac64d5258b83e2ef719b410f0
Natalia Gherardi <natirgh@gmail.com>
2024-02-26 18:11:59 -0300

    add build workflow and remove compilation errors

--- src/api/animals.controller.ts
+++ src/api/animals.controller.ts
@@ -1,12 +1,6 @@
 import type { Request, Response } from 'express'
 import { animalsData } from '../data/animals-data'
 
-const animalsDataReceived = [...animalsData]
-
-if (typeof animalsData = 'array') {
-  animalsData.sort()
-}
-
-export const getAnimals = (_req: Request, res: Response): void => {
+export const getAnimals = async (_req: Request, res: Response): Promise<void> => {
   res.status(200).json(animalsData)
 }
