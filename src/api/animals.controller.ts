6639d4f0402dc22fc96a50c28556043ccb95d22c
Natalia Gherardi <natirgh@gmail.com>
2024-03-11 11:01:45 -0300

    feat(filter): add endpoint to filter animals

--- src/api/animals.controller.ts
+++ src/api/animals.controller.ts
@@ -1,6 +1,16 @@
 import type { Request, Response } from 'express'
-import { animalsData } from '../data/animals-data'
+import { animalsData, type Animal } from '../data/animals-data'
 
 export const getAnimals = (_req: Request, res: Response): void => {
   res.status(200).json(animalsData)
+}
+
+const filterAnimals = (animals: Animal[], filter: string): Animal[] => {
+  return animals.filter(animal => animal.name.toLowerCase().includes(filter.toLowerCase()))
+}
+
+export const getAnimalsByFilter = (req: Request, res: Response): void => {
+  const filter = req.params.filter
+  const filteredAnimals = filterAnimals(animalsData, filter)
+  res.status(200).json(filteredAnimals)
 }
